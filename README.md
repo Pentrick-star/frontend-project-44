f121b8037c69f5be3614800447e11269d63414e0
Pentrick <bibenina09@mail.ru>
2025-05-26 23:02:02 +0300

    добавлена игра brain-even и аскинема

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
+[![Quality Gate Status](https://sonarcloud.io/api/project_badges/measure?project=Pentrick-star_frontend-project-44&metric=alert_status)](https://sonarcloud.io/summary/new_code?id=Pentrick-star_frontend-project-44)
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/Pentrick-star/frontend-project-44/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/Pentrick-star/frontend-project-44/actions)
+Демонстрация игры: https://asciinema.org/a/abc12345
---
+++ src/games/even.js
@@ -0,0 +1,31 @@
+import readlineSync from 'readline-sync';
+
+const isEven = (num) => num % 2 === 0;
+
+const runEvenGame = () => {
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+  const roundsCount = 3;
+  for (let i = 0; i < roundsCount; i += 1) {
+    const question = Math.floor(Math.random() * 100);
+    console.log(`Question: ${question}`);
+    const answer = readlineSync.question('Your answer: ');
+
+    const correctAnswer = isEven(question) ? 'yes' : 'no';
+
+    if (answer !== correctAnswer) {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      console.log(`Let's try again, ${name}!`);
+      return;
+    }
+
+    console.log('Correct!');
+  }
+
+  console.log(`Congratulations, ${name}!`);
+};
+
+export default runEvenGame;
