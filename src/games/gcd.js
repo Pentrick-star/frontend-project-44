2453298307000fae5b64d93fd69b4fa53a96954e
Pentrick <bibenina09@mail.ru>
2025-05-30 02:38:38 +0300

    финальные правки линтера

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,21 +1,21 @@
-import getRandomNumber from '../utils.js'
-import runGame from '../index.js'
+import getRandomNumber from '../utils.js';
+import runGame from '../index.js';
 
 const findGCD = (a, b) => {
-  if (b === 0) return a
-  return findGCD(b, a % b)
-}
+  if (b === 0) return a;
+  return findGCD(b, a % b);
+};
 
 const generateRound = () => {
-  const num1 = getRandomNumber(1, 100)
-  const num2 = getRandomNumber(1, 100)
-  const question = `${num1} ${num2}`
-  const correctAnswer = String(findGCD(num1, num2))
-  return [question, correctAnswer]
-}
+  const num1 = getRandomNumber(1, 100);
+  const num2 = getRandomNumber(1, 100);
+  const question = `${num1} ${num2}`;
+  const correctAnswer = String(findGCD(num1, num2));
+  return [question, correctAnswer];
+};
 
 const runGCDGame = () => {
-  runGame('Find the greatest common divisor of given numbers.', generateRound)
-}
+  runGame('Find the greatest common divisor of given numbers.', generateRound);
+};
 
-export default runGCDGame
+export default runGCDGame;
--- src/games/prime.js
+++ src/games/prime.js
@@ -1,22 +1,22 @@
-import getRandomNumber from '../utils.js'
-import runGame from '../index.js'
+import getRandomNumber from '../utils.js';
+import runGame from '../index.js';
 
 const isPrime = (num) => {
-  if (num < 2) return false
+  if (num < 2) return false;
   for (let i = 2; i <= Math.sqrt(num); i += 1) {
-    if (num % i === 0) return false
+    if (num % i === 0) return false;
   }
-  return true
-}
+  return true;
+};
 
 const generateRound = () => {
-  const question = getRandomNumber(2, 100)
-  const correctAnswer = isPrime(question) ? 'yes' : 'no'
-  return [question, correctAnswer]
-}
+  const question = getRandomNumber(2, 100);
+  const correctAnswer = isPrime(question) ? 'yes' : 'no';
+  return [question, correctAnswer];
+};
 
 const runPrimeGame = () => {
-  runGame('Answer "yes" if given number is prime. Otherwise answer "no".', generateRound)
-}
+  runGame('Answer "yes" if given number is prime. Otherwise answer "no".', generateRound);
+};
 
-export default runPrimeGame
+export default runPrimeGame;
