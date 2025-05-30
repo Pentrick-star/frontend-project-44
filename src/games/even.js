341f365fccefe9d888dfefc7a18bb8201dcac9d9
pentrick <bibenina09@mail.ru>
2025-05-30 23:32:49 +0300

    Update even.js

--- src/games/even.js
+++ src/games/even.js
@@ -1,7 +1,7 @@
 import getRandomNumber from '../utils.js'
 import runGame from '../index.js'
 
-const isEven = (num) => num % 2 === 0
+const isEven = num => num % 2 === 0
 
 const generateRound = () => {
   const question = getRandomNumber(1, 100)
