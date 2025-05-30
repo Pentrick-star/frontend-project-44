a3cb6109399b652fcd74213a9b1f5b96f2d76358
pentrick <bibenina09@mail.ru>
2025-05-30 23:50:15 +0300

    Update progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -14,7 +14,6 @@ const generateRound = () => {
   const progressionWithHidden = progression
     .map((num, idx) => (idx === hiddenIndex ? '..' : num))
     .join(' ')
-  
   return [progressionWithHidden, correctAnswer]
 }
 
